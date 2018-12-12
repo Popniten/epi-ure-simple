b07d6a66119d5ca492a487e15575552b9f046053
Popniten <pe222gq@student.lnu.se>
2018-12-12 22:20:20 +0100

    Breaking out header.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <Navigation msg="Navigation Component"/>
+    <Header />
     <router-view/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navigation from '@/components/Navigation.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   components: {
-    Navigation
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Header
+    <Navigation msg="Navigation Component"/>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
